8a326a864dec42c22cddf14c12d4c28eb322216a
20190981 <ybmdeer1@naver.com>
2026-05-01 15:12:36 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+import { writeFileSync } from 'node:fs';
+import Parser from "rss-parser";
+
+/**
+ * README.MD에 작성될 페이지 텍스트
+ * @type {string}
+ */
+let text = `# Hi there 👋
+
+## 이런 환경에 익숙해요✍🏼
+
+## 언어
+
+<p>
+  <img alt="Python" src="https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=Python&logoColor=white"/>
+  <img alt="Django" src="https://img.shields.io/badge/Django-092E20?style=flat-square&logo=Django&logoColor=white"/>
+  <img alt="SQL" src="https://img.shields.io/badge/SQL-4479A1?style=flat-square&logo=Postgresql&logoColor=white"/>
+  <img alt="AWS" src="https://img.shields.io/badge/AWS-232F3E?style=flat-square&logo=Amazon%20AWS&logoColor=white"/>
+  <img alt="ML" src="https://img.shields.io/badge/Machine%20Learning-3776AB?style=flat-square&logo=scikit-learn&logoColor=white"/>
+</p>
+
+## 📕 Latest Blog Posts
+
+`;
+
+// rss-parser 생성
+const parser = new Parser({
+    headers: {
+        Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
+    }});
+
+(async () => {
+
+    // 피드 목록
+    const feed = await parser.parseURL('https://yujung820.tistory.com/rss'); 
+    
+    text += `<ul>`;
+    
+    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 10; i++) {
+        const {title, link} = feed.items[i];
+        console.log(`${i + 1}번째 게시물`);
+        console.log(`추가될 제목: ${title}`);
+        console.log(`추가될 링크: ${link}`);
+        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+    }
+
+    text += `</ul>`;
+    
+    // README.md 파일 생성
+    writeFileSync('README.md', text, 'utf8', (e) => {
+        console.log(e);
+    })
+    console.log('업데이트 완료');
+})();
